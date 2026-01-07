d04fb822cb1451dbb192b4967f9693b8c7818e49
maya-katsir <mayka1573@gmail.com>
2026-01-07 19:15:22 +0200

    added new route for get all posts - also by sender

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -4,20 +4,26 @@ import PostRepository from '../repositories/postRepository';
 class PostController {
     async createPost(req: Request, res: Response) {
         try {
-             const { sender, content, title } = req.body;
-    if (!sender || !title) {
-        res.status(400).send({ message: 'body param is missing (sender or title)' });
-        return;
-    }
-    const post = await PostRepository.createPost(title, sender, content);
-    res.status(200).send(post);
-}
-         catch (err) {
+            const { sender, content, title } = req.body;
+            if (!sender || !title) {
+                res.status(400).send({ message: 'body param is missing (sender or title)' });
+                return;
+            }
+            const post = await PostRepository.createPost(title, sender, content);
+            res.status(200).send(post);
+        } catch (err) {
             console.error('Error creating post', err);
 
             return res.status(500).json({ message: 'Internal server error' });
         }
-    }
+    };
+    async getAllPosts(req: Request<{}, {}, {}, Record<string, string | undefined>>, res: Response) {
+        try {
+            res.status(200).send({ posts: await PostRepository.getAllPosts(req.query) });
+        } catch (err) {
+            console.error('Failed getting all posts', err);
+        }
+    };
 }
 
-export default new PostController();
+export const postController = new PostController();
--- src/repositories/postRepository.ts
+++ src/repositories/postRepository.ts
@@ -1,8 +1,17 @@
-import { Comment, commentModel } from "../models/comment";
+import { FilterQuery } from "mongoose";
 import { Post, postModel } from "../models/post";
 
 class PostRepository {
-  async createPost(title: string, sender: string, content?: string) {await postModel.create({ title, sender, content });}
+  async createPost(title: string, sender: string, content?: string) {await postModel.create({ title, sender, content });};
+  async getAllPosts(query: Record<string, string | undefined>) {
+    const filters: FilterQuery<Post> = {};
+
+    if ('sender' in query) {
+        filters.sender = query.sender;
+    }
+
+    return await postModel.find(filters).select('-__v');
+  };
 }
 
 export default new PostRepository();
